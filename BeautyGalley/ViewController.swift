123bf3deed17b4ff5037e3ad75cd0939bf0e3ae2
Stephen Ge <stvea@promote.cache-dns.local>
2017-01-29 14:23:39 +0800

    Initial Commit

---
+++ BeautyGalley/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyGalley
+//
+//  Created by Stephen Ge on 2017/1/29.
+//  Copyright © 2017年 Stephen Ge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
